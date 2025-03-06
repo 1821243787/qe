5711e3694d992c9e07abbc23e25ebfb4b474617f
1821243787 <1821243787@qq.com>
2025-03-06 20:40:27 +0800

    55

---
+++ js/manifest.js
@@ -0,0 +1,16 @@
+(function () {  
+
+_mrmcp = (typeof _mrmcp == 'undefined') ? {} : _mrmcp;
+_mrmcp['campaign_id'] = 'none';
+_mrmcp['owner_id'] = '67c970a4aec720bbd905b0f4';
+_mrmcp['creative_id'] = '67c974795c31f6c22807e761';
+_mrmcp['ga_url'] = _mrmcp['ga_url'] || (('https:' == document.location.protocol ? 'https://' : 'http://') + 'card.mugeda.com/weixin/card/ga.js');
+_mrmcp['width'] = _mrmcp['width'] || 320;
+_mrmcp['height'] = _mrmcp['height'] || 626;
+_mrmcp['type'] = 'smart';
+_mrmcp['title'] = 'dd';
+_mrmcp['build_number'] = parseInt('338');
+_mrmcp['publish_time'] = 'Thu Mar 06 2025 20:33:39 GMT+0800 (China Standard Time)';
+_mrmcp['track_bot'] = 'http://cdn.mugeda.com/media/pages/track/track_20131030.html';
+_mrmcp['data_server'] = "https://weika.mugeda.com";
+})();
